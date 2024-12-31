d50268c7f6ba793066e2479ec662cd51dec4978d
unknown <danilovieira37@icloud.com>
2024-12-31 20:35:57 -0300

    adicionar exemplo 02

---
+++ aula/exemplo_02.py
@@ -0,0 +1,5 @@
+# Criar um programa onde o usuário digita dois valores e apareça a soma.
+
+valor_1 = int(input('Digite o primeiro valor a ser somado: '))
+valor_2 = int(input('Digite o segundo valor a ser somado: '))
+print(valor_1 + valor_2)
